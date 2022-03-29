7b8fc07fc08d3a4a9e2a2ec2e0e80a4018d7fc90
a19guillermobf <a19guillermobf@iessanclemente.net>
2022-03-29 21:58:39 +0200

    2022-03-29 JSON com Fetch

--- AJAX/Fetch/scripts/myscript.js
+++ AJAX/Fetch/scripts/myscript.js
@@ -4,5 +4,18 @@ const fragmento = document.createDocumentFragment()
 
 /**Esto hai que revisa-lo, que nom entendim moi bem como vai */
 fetch("https://jsonplaceholder.typicode.com/users",{method:"GET"})
-.then(resposta=>console.log(resposta))
-.catch(erro=>console.log("Erro!!"))
+.then(resposta=>{
+    if (resposta.ok) 
+        return resposta.json()
+     else
+        return Promise.reject("Mensagem de erro")
+    })
+.then(json=>{
+    json.forEach(el=>{
+        const li=document.createElement("li")
+        li.innerHTML=`${el.name} - ${el.email} -- ${el.phone}`
+        fragmento.appendChild(li)
+    })
+    lista.appendChild(fragmento)
+})
+.catch(erro=>console.log(erro))
